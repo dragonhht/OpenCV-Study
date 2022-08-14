ae3c37542071001d09c4d906931f043d923aa941
dragonhht <dragon_hht@163.com>
2022-08-14 22:38:12 +0800

    像素点操作

--- src/img/img.go
+++ src/img/img.go
@@ -66,3 +66,47 @@ func ConvertColor(path string) {
 	// 保存灰度图片
 	WriteImg("./灰度图.jpeg", gray)
 }
+
+// PixelVisit 像素处理
+func PixelVisit(path string) {
+	// 读取图片
+	img := ReadImg(path)
+	defer img.Close()
+	//gray := gocv.NewMat()
+	//defer gray.Close()
+	//gocv.CvtColor(img, &gray, gocv.ColorBGRToGray)
+
+	// 获取长、宽及通道数
+	rows := img.Rows()
+	cols := img.Cols()
+	channels := img.Channels()
+
+	// 创建空白的Mat对象
+	newImg := gocv.Zeros(rows, cols, img.Type())
+	defer newImg.Close()
+
+	for row := 0; row < rows; row++ {
+		for col := 0; col < cols; col++ {
+			// 灰度只有1个通道
+			if channels == 1 {
+				// 获取并修改像素值
+				pv := img.GetUCharAt(row, col)
+				newImg.SetUCharAt(row, col, pv)
+				continue
+			}
+			// 彩色为3通道
+			if channels == 3 {
+				// 获取并修改像素值
+				vecb := img.GetVecbAt(row, col)
+				for c := 0; c < channels; c++ {
+					newImg.SetUCharAt(row, col*channels+c, vecb[c])
+				}
+			}
+		}
+	}
+
+	// 增加亮度
+	newImg.AddUChar(50)
+	// 显示新图像
+	ShowImg("新图像", newImg)
+}
--- src/img/img_test.go
+++ src/img/img_test.go
@@ -5,5 +5,5 @@ import "testing"
 const imgPath = "/home/huang/下载/09c60131bf1dee61dad8d6995d42059e.jpeg"
 
 func TestROI(t *testing.T) {
-	ConvertColor(imgPath)
+	PixelVisit(imgPath)
 }
